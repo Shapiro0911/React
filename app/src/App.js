f2f98d51144d523f0d842669acf8ece7e3aa7d41
Max <maxbui0911@gmail.com>
2021-11-01 15:12:07 +0200

    L2

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,33 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import { Message } from './components/Message/Message'
+//import { Message } from './components/Message/Message'
+import { Form } from './components/Form/Form'
 
 function App() {
-  let text = 'Hi';
+  const [messages, setMessages] = useState([]);
+
+  const handleMessage = (text) => {
+    if (messages.length === 0) {
+      setMessages([{ id: 0, text: text, author: 'human' }]);
+    }
+    else {
+      setMessages([...messages, { id: messages[messages.length - 1].id + 1, text: text, author: 'human' }]);
+    }
+  }
+
+  useEffect(() => {
+    if (messages.length !== 0 && messages[messages.length - 1].author === "human") {
+      setTimeout(() => {
+        setMessages([...messages, { id: messages[messages.length - 1].id + 1, text: "Hello", author: 'bot' }])
+      }, 1500)
+    }
+  }, [messages])
+
   return (
     <div className="App">
       <header className="App-header">
-        <Message message={text} />
+        {messages.map((message) => <div key={message.id}>{message.text}</div>)}
+        <Form sendMessage={handleMessage} />
       </header>
     </div>
   );
